04014bf2df70b0f2b04661d438874c11b0413b0c
Alex Ellis <alexellis2@gmail.com>
2019-07-10 16:56:21 +0100

    Update

--- gitops/deploy.sh
+++ gitops/deploy.sh
@@ -15,3 +15,25 @@ then
     --template=${TEMPLATE} \
     --ssh="${SSH_KEY_ID}"
 fi
+
+sleep 30
+
+# Instance takes 40 secs +/- to build
+for i in {0..120}; do
+    civo instance show "${HOSTNAME}" > instance.txt
+    grep "ACTIVE" instance.txt
+
+    if [ "$?" -eq "0" ]
+    then
+        echo "VM found, and active"
+
+        export IP=$(grep "Public IP" instance.txt | cut -d ">" -f2 |tr -d " ") 
+        echo $IP
+        ssh -oStrictHostKeyChecking=no root@$IP "cat /etc/os-release"
+
+        break
+    fi
+
+    sleep 10
+done
+
