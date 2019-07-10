85e163fe8fd26a2ffd153c574341b4f0ed4be0d9
Alex Ellis <alexellis2@gmail.com>
2019-07-10 16:17:30 +0100

    Remove preinstall
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gitops/prepare.sh
+++ gitops/prepare.sh
@@ -1,19 +1,10 @@
+language: ruby
 
-#!/bin/bash
+services:
+  - docker
 
-sudo gem install civo_cli
-civo apikey add production "${CIVO_API_KEY}"
-civo apikey current production
+install:
+- ./gitops/prepare.sh
 
-civo instance list
-
-mkdir -p ~/.ssh/
-
-
-# Convert the text back into human-readable format:
-echo -n $SSH_PUBLIC_KEY | base64 --decode > ~/.ssh/id_rsa.pub
-echo -n $SSH_PRIVATE_KEY | base64 --decode > ~/.ssh/id_rsa
-
-echo -n $SSH_KEY_ID > $HOME/key_ids.txt
-
-echo `pwd`
+script:
+- ./gitops/deploy.sh
