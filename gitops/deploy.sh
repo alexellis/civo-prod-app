dc9fa152942dfb56883061d20ecd317e12ca6af1
Alex Ellis <alexellis2@gmail.com>
2019-07-10 16:46:43 +0100

    Create VM if it doesn't exist
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gitops/deploy.sh
+++ gitops/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-echo `pwd`
+export HOSTNAME="gitops-prod.example.com"
 
 civo apikey current production
-civo instance list
+
+civo instance show "${HOSTNAME}"
+export EXISTS=$?
+export TEMPLATE="811a8dfb-8202-49ad-b1ef-1e6320b20497"
+
+if [ "${EXISTS}" -ne "0" ]
+then
+    civo instance create \
+    ${HOSTNAME} \
+    --template=${TEMPLATE} \
+    --ssh="${SSH_KEY_ID}"
+fi
