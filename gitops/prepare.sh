b1b8c76b0565ac72abae8f5bfe9dce3407835ffa
Alex Ellis <alexellis2@gmail.com>
2019-07-10 16:14:56 +0100

    Check instances in the prepare stage
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gitops/prepare.sh
+++ gitops/prepare.sh
@@ -1,8 +1,11 @@
+
 #!/bin/bash
 
 sudo gem install civo_cli
-civo apikey add production $CIVO_API_KEY
+civo apikey add production "${CIVO_API_KEY}"
 civo apikey current production
+
+civo instance list
 
 mkdir -p ~/.ssh/
 
@@ -14,4 +17,3 @@ echo -n $SSH_PRIVATE_KEY | base64 --decode > ~/.ssh/id_rsa
 echo -n $SSH_KEY_ID > $HOME/key_ids.txt
 
 echo `pwd`
-
