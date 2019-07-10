d445f5217e44693bb456bb3202afd5b5715e94e1
Alex Ellis <alexellis2@gmail.com>
2019-07-10 16:08:18 +0100

    Install CLI with sudo
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- gitops/prepare.sh
+++ gitops/prepare.sh
@@ -1,13 +1,14 @@
+#!/bin/bash
 
-gem install civo_cli
+sudo gem install civo_cli
 civo apikey add production $CIVO_API_KEY
 civo apikey current production
 
 mkdir -p ~/.ssh/
 
-echo -n $SSH_PUBLIC_KEY > ~/.ssh/id_rsa.pub
 
-# Convert the single line back into multiple lines
+# Convert the text back into human-readable format:
+echo -n $SSH_PUBLIC_KEY | base64 --decode > ~/.ssh/id_rsa.pub
 echo -n $SSH_PRIVATE_KEY | base64 --decode > ~/.ssh/id_rsa
 
 echo -n $SSH_KEY_ID > $HOME/key_ids.txt
